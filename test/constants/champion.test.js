bd040b101b2ade1aa0af17f3917b87d05990e9ca
Julio Sansossio <J.Sansossio@payvision.com>
2019-09-20 13:06:08 +0200

    Champions constants

---
+++ test/constants/champion.test.js
@@ -0,0 +1,29 @@
+const {
+  describe,
+  it
+} = require('mocha')
+const { expect } = require('chai')
+const { getChampionName } = require('../../src//constants/champions')
+
+describe('Champions Constant', () => {
+  it('should return a empty champ', () => {
+    const response = getChampionName(0)
+    expect(response).to.eq('EmptyChampion')
+  })
+  it('should return a champ', () => {
+    const response = getChampionName(1)
+    expect(typeof response).to.eq('string')
+  })
+  it('should return error unknow champion', (done) => {
+    try {
+      const response = getChampionName(-1)
+      done(new Error(`Unexpected response: ${response}`))
+    } catch (e) {
+      if (e instanceof Error) {
+        done()
+        return
+      }
+      done(new Error(`Unexpected error type: ${typeof e}`))
+    }
+  })
+})

a59dd8f0090b8893ca188cdf7305a9400a66505d
Colin Cheung <ColinCee@users.noreply.github.com>
2020-10-29 02:48:18 +0000

    initial work

--- src/apis/tft/tft.ts
+++ src/apis/tft/tft.ts
@@ -2,6 +2,7 @@ import { BaseApiTft } from './base/base.api.tft'
 import { MatchTFTApi } from './match/match.tft'
 import { SummonerTftApi } from './summoner/summoner'
 import { TFTStaticFiles } from './static/static'
+import { LeagueTFTApi } from './league/league.tft'
 
 /**
  * TFT Api
@@ -15,6 +16,10 @@ export class TftApi extends BaseApiTft {
    * Summoner methods
    */
   public readonly Summoner = new SummonerTftApi(this.getParam())
+  /**
+   * League methods
+   */
+  public readonly League = new LeagueTFTApi(this.getParam())
   /**
    * Static files
    */

43b07933b8f62f4308ee393c5e34901d804f4c2e
Julio Sansossio <juliosansossio@gmail.com>
2021-09-14 20:31:12 +0200

    add regions test

---
+++ src/constants/regions.test.ts
@@ -0,0 +1,29 @@
+import { RegionGroups, Regions, regionToRegionGroup } from '.'
+
+describe('Regions', () => {
+  describe('regionToRegionGroup', () => {
+    it('should match americas', () => {
+      expect(regionToRegionGroup(Regions.AMERICA_NORTH)).toEqual(RegionGroups.AMERICAS)
+      expect(regionToRegionGroup(Regions.BRAZIL)).toEqual(RegionGroups.AMERICAS)
+      expect(regionToRegionGroup(Regions.LAT_NORTH)).toEqual(RegionGroups.AMERICAS)
+      expect(regionToRegionGroup(Regions.LAT_SOUTH)).toEqual(RegionGroups.AMERICAS)
+      expect(regionToRegionGroup(Regions.OCEANIA)).toEqual(RegionGroups.AMERICAS)
+    })
+
+    it('should match europe', () => {
+      expect(regionToRegionGroup(Regions.EU_EAST)).toEqual(RegionGroups.EUROPE)
+      expect(regionToRegionGroup(Regions.EU_WEST)).toEqual(RegionGroups.EUROPE)
+      expect(regionToRegionGroup(Regions.TURKEY)).toEqual(RegionGroups.EUROPE)
+      expect(regionToRegionGroup(Regions.RUSSIA)).toEqual(RegionGroups.EUROPE)
+    })
+
+    it('should match asia', () => {
+      expect(regionToRegionGroup(Regions.JAPAN)).toEqual(RegionGroups.ASIA)
+      expect(regionToRegionGroup(Regions.KOREA)).toEqual(RegionGroups.ASIA)
+    })
+
+    it('should throw error when the region does not exists', () => {
+      expect(() => regionToRegionGroup('not a region' as any)).toThrowError()
+    })
+  })
+})

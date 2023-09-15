a671d140cec37e2710bde7658454845369e27a82
Rleaf <calcifer44@gmail.com>
2023-09-15 01:49:47 -0400

    update region constants

--- src/constants/regions.test.ts
+++ src/constants/regions.test.ts
@@ -23,6 +23,11 @@ describe('Regions', () => {
 
     it('should match sea', () => {
       expect(regionToRegionGroup(Regions.OCEANIA)).toEqual(RegionGroups.SEA)
+      expect(regionToRegionGroup(Regions.PHILIPPINES)).toEqual(RegionGroups.SEA)
+      expect(regionToRegionGroup(Regions.SINGAPORE)).toEqual(RegionGroups.SEA)
+      expect(regionToRegionGroup(Regions.THAILAND)).toEqual(RegionGroups.SEA)
+      expect(regionToRegionGroup(Regions.TAIWAN)).toEqual(RegionGroups.SEA)
+      expect(regionToRegionGroup(Regions.VIETNAM)).toEqual(RegionGroups.SEA)
     })
 
     it('should throw error when the region does not exists', () => {

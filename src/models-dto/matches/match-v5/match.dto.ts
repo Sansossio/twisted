e57e33c2b46c3fd1d8b7406bdc556c3dd2ba6d7b
victorhurchella <victor.urchella@outlook.com>
2023-10-17 12:22:30 -0300

    type(matchV5): add ChallengeDto in ParticipantDto

--- src/models-dto/matches/match-v5/match.dto.ts
+++ src/models-dto/matches/match-v5/match.dto.ts
@@ -55,6 +55,7 @@ export namespace MatchV5DTOs {
     assists: number
     baronKills: number
     bountyLevel: number
+    challenges: ChallengesDto
     champExperience: number
     champLevel: number
     championId: number
@@ -166,6 +167,121 @@ export namespace MatchV5DTOs {
     wardsKilled: number
     wardsPlaced: number
     win: boolean
+  }
+
+  export interface ChallengesDto {
+    "12AssistStreakCount": number
+    abilityUses: number
+    acesBefore15Minutes: number
+    alliedJungleMonsterKills: number
+    baronTakedowns: number
+    blastConeOppositeOpponentCount: number
+    bountyGold: number
+    buffsStolen: number
+    completeSupportQuestInTime: number
+    controlWardsPlaced: number
+    damagePerMinute: number
+    damageTakenOnTeamPercentage: number
+    dancedWithRiftHerald: number
+    deathsByEnemyChamps: number
+    dodgeSkillShotsSmallWindow: number
+    doubleAces: number
+    dragonTakedowns: number
+    earlyLaningPhaseGoldExpAdvantage: number
+    effectiveHealAndShielding: number
+    elderDragonKillsWithOpposingSoul: number
+    elderDragonMultikills: number
+    enemyChampionImmobilizations: number
+    enemyJungleMonsterKills: number
+    epicMonsterKillsNearEnemyJungler: number
+    epicMonsterKillsWithin30SecondsOfSpawn: number
+    epicMonsterSteals: number
+    epicMonsterStolenWithoutSmite: number
+    firstTurretKilled: number
+    firstTurretKilledTime: number
+    flawlessAces: number
+    fullTeamTakedown: number
+    gameLength: number
+    goldPerMinute: number
+    hadOpenNexus: number
+    immobilizeAndKillWithAlly: number
+    initialBuffCount: number
+    initialCrabCount: number
+    jungleCsBefore10Minutes: number
+    junglerKillsEarlyJungle: number
+    junglerTakedownsNearDamagedEpicMonster: number
+    kTurretsDestroyedBeforePlatesFall: number
+    kda: number
+    killAfterHiddenWithAlly: number
+    killParticipation: number
+    killedChampTookFullTeamDamageSurvived: number
+    killingSprees: number
+    killsNearEnemyTurret: number
+    killsOnLanersEarlyJungleAsJungler: number
+    killsOnRecentlyHealedByAramPack: number
+    killsUnderOwnTurret: number
+    killsWithHelpFromEpicMonster: number
+    knockEnemyIntoTeamAndKill: number
+    landSkillShotsEarlyGame: number
+    laneMinionsFirst10Minutes: number
+    laningPhaseGoldExpAdvantage: number
+    legendaryCount: number
+    lostAnInhibitor: number
+    maxCsAdvantageOnLaneOpponent: number
+    maxKillDeficit: number
+    maxLevelLeadLaneOpponent: number
+    mejaisFullStackInTime: number
+    moreEnemyJungleThanOpponent: number
+    multiKillOneSpell: number
+    multiTurretRiftHeraldCount: number
+    multikills: number
+    multikillsAfterAggressiveFlash: number
+    mythicItemUsed: number
+    outerTurretExecutesBefore10Minutes: number
+    outnumberedKills: number
+    outnumberedNexusKill: number
+    perfectDragonSoulsTaken: number
+    perfectGame: number
+    pickKillWithAlly: number
+    playedChampSelectPosition: number
+    poroExplosions: number
+    quickCleanse: number
+    quickFirstTurret: number
+    quickSoloKills: number
+    riftHeraldTakedowns: number
+    saveAllyFromDeath: number
+    scuttleCrabKills: number
+    skillshotsDodged: number
+    skillshotsHit: number
+    snowballsHit: number
+    soloBaronKills: number
+    soloKills: number
+    stealthWardsPlaced: number
+    survivedSingleDigitHpCount: number
+    survivedThreeImmobilizesInFight: number
+    takedownOnFirstTurret: number
+    takedowns: number
+    takedownsAfterGainingLevelAdvantage: number
+    takedownsBeforeJungleMinionSpawn: number
+    takedownsFirstXMinutes: number
+    takedownsInAlcove: number
+    takedownsInEnemyFountain: number
+    teamBaronKills: number
+    teamDamagePercentage: number
+    teamElderDragonKills: number
+    teamRiftHeraldKills: number
+    tookLargeDamageSurvived: number
+    turretPlatesTaken: number
+    turretTakedowns: number
+    turretsTakenWithRiftHerald: number
+    twentyMinionsIn3SecondsCount: number
+    twoWardsOneSweeperCount: number
+    unseenRecalls: number
+    visionScoreAdvantageLaneOpponent: number
+    visionScorePerMinute: number
+    wardTakedowns: number
+    wardTakedownsBefore20M: number
+    wardsGuarded: number
   }
 
   export interface PerksDto {

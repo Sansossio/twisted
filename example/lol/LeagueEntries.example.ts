1afe05df929c185184645cf226378d38c6225f86
Julio Sansossio <juliosansossio@gmail.com>
2024-03-20 10:46:55 -0400

    106-not-sure-whats-happening-here (#110)
    * 106-not-sure-whats-happening-here
    
    * remove useless log
    
    * dont autorun

--- example/lol/LeagueEntries.example.ts
+++ example/lol/LeagueEntries.example.ts
@@ -3,5 +3,7 @@ import { Queues, Tiers, Divisions, Regions } from '../../src/constants'
 
 export async function leagueEntriesExample () {
   const api = new LolApi()
-  return await api.League.entries(Queues.RANKED_SOLO_5x5, Tiers.BRONZE, Divisions.I, Regions.AMERICA_NORTH)
+  const { response: entries } = await api.League.entries(Queues.RANKED_SOLO_5x5, Tiers.BRONZE, Divisions.I, Regions.LAT_NORTH)
+
+  return entries;
 }

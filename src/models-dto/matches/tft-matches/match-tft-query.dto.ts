da623d92fa79a5c1de14cd6ca7d3f4a7b2d8cfca
José Alves <jose.amiginho@hotmail.com>
2024-05-17 00:20:28 +0200

    Update match-tft-query.dto.ts with missing query params (#116)
    * Update match-tft-query.dto.ts with missing query params
    
    Update of query params available on RIOT API for match request (https://developer.riotgames.com/apis#tft-match-v1/GET_getMatchIdsByPUUID).
    
    Added the following options:
    - start (Defaults to 0. Start index.)
    - startTime (Epoch timestamp in seconds. The matchlist started storing timestamps on June 16th, 2021. Any matches played before June 16th, 2021 won't be included in the results if the startTime filter is set.)
    - endTime (Epoch timestamp in seconds.)
    
    * increment version

--- src/models-dto/matches/tft-matches/match-tft-query.dto.ts
+++ src/models-dto/matches/tft-matches/match-tft-query.dto.ts
@@ -3,4 +3,19 @@ export class MatchTFTQueryDTO {
    * Number of TFT Matches to list
    */
   count?: number
+
+  /**
+   * Ignore the first respective TFT Matches. Defaults to 0.
+   */
+  start?: number
+
+  /**
+   * Epoch timestamp in seconds. The matchlist started storing timestamps on June 16th, 2021. Any matches played before June 16th, 2021 won't be included in the results if the startTime filter is set.
+   */
+  startTime?: number
+
+  /**
+   * Epoch timestamp in seconds.
+   */
+  endTime?: number
 }

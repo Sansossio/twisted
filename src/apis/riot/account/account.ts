f2bf9d61f7cd132f6fa78acb8338972d4de435de
Jose Alves <jose.amiginho@hotmail.com>
2024-10-23 17:31:47 +0200

    fix param docs for getByRiotId

--- src/apis/riot/account/account.ts
+++ src/apis/riot/account/account.ts
@@ -1,30 +1,40 @@
-import { RegionGroups } from '../../../constants'
-import { endpointsRiotV1 } from '../../../endpoints/endpoints'
-import { BaseApiRiot } from '../base/base.api.riot'
-import { AccountDto } from '../../../models-dto/account/account.dto'
+import { RegionGroups } from "../../../constants";
+import { endpointsRiotV1 } from "../../../endpoints/endpoints";
+import { BaseApiRiot } from "../base/base.api.riot";
+import { AccountDto } from "../../../models-dto/account/account.dto";
 
 export class AccountV1Api extends BaseApiRiot {
-   /** 
+  /**
    * Get by PUUID
    * @param puuid
-   * 
+   *
    */
-   public async getByPUUID(puuid: string, region: RegionGroups) {
-      const params = {
-         summonerPUUID: puuid
-      }
-      return this.request<AccountDto>(region, endpointsRiotV1.AccountPUUID, params)
-   }
-   /**
+  public async getByPUUID(puuid: string, region: RegionGroups) {
+    const params = {
+      summonerPUUID: puuid,
+    };
+    return this.request<AccountDto>(
+      region,
+      endpointsRiotV1.AccountPUUID,
+      params
+    );
+  }
+  /**
    * Get by RiotId
-   * @param puuid
-   * 
+   * @param gameName
+   * @param tagLine
+   * @param region
+   *
    */
-   public async getByRiotId(gameName: string, tagLine: string, region: RegionGroups) {
-      const params = {
-         gameName,
-         tagLine
-      }
-      return this.request<AccountDto>(region, endpointsRiotV1.RiotId, params)
-   }
+  public async getByRiotId(
+    gameName: string,
+    tagLine: string,
+    region: RegionGroups
+  ) {
+    const params = {
+      gameName,
+      tagLine,
+    };
+    return this.request<AccountDto>(region, endpointsRiotV1.RiotId, params);
+  }
 }

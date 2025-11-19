45df018beebc638b8ca2ae5029fb6a8df7410079
Ahmed Rangel <ahmedrangel@outlook.com>
2025-11-19 06:54:28 -0500

    fix: replays example

--- example/lol/MatchV5Replays.example.ts
+++ example/lol/MatchV5Replays.example.ts
@@ -1,6 +1,6 @@
 import { RiotApi } from '../../src'
 import { LolApi } from '../../src'
-import { AccountAPIRegionGroups } from '../../src/constants'
+import { AccountAPIRegionGroups, RegionGroups } from '../../src/constants'
 import { config } from '../config/config'
 
 export async function matchV5ReplaysExample() {
@@ -8,7 +8,7 @@ export async function matchV5ReplaysExample() {
    const rApi = new RiotApi()
 
    const { response: { puuid }} = await rApi.Account.getByRiotId(config.summonerName, config.tagLine, config.regionGroup as AccountAPIRegionGroups)
-   const { response: data } = await api.MatchV5.replays(puuid, config.regionGroup)
+   const { response: data } = await api.MatchV5.replays(puuid, RegionGroups.EUROPE)
    console.log(data);
 }
 
